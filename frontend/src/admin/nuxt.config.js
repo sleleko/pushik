0c7b126bdb5b5335b66c87ad58bc84425c8c49eb
Sergey Leleko <sergey@leleko.ru>
2022-04-18 21:13:25 +0300

    init

---
+++ frontend/src/admin/nuxt.config.js
@@ -0,0 +1,42 @@
+import {merge, union} from 'lodash'
+import {Config, findEnv, loadEnv} from '@vesp/frontend'
+
+Config.ssr = false
+Config.srcDir = __dirname
+Config.buildDir = '.nuxt/admin'
+Config.generate = {
+  dir: 'dist/admin',
+  exclude: [/^\//],
+}
+
+const env = loadEnv(findEnv('../'))
+Config.axios.baseURL = env.API_URL || '/api/'
+Config.head.title = env.APP_NAME || 'Vesp Framework'
+
+Config.modules = union(Config.modules, ['@vesp/frontend', '@nuxtjs/auth-next'])
+
+Config.router = merge(Config.router, {
+  base: '/admin/',
+  middleware: ['auth'],
+})
+
+// Specify vueI18n config as a function for smooth development
+Config.i18n.vueI18n = '@/lexicons/index.js'
+
+Config.fontawesome = merge(Config.fontawesome, {
+  icons: {
+    solid: union(Config.fontawesome.icons.solid, ['faUsers', 'faArrowLeft', 'faSignOutAlt']),
+  },
+})
+
+Config.bootstrapVue = merge(Config.bootstrapVue, {
+  componentPlugins: union(Config.bootstrapVue.componentPlugins, [
+    'ImagePlugin',
+    'FormCheckboxPlugin',
+    'FormTagsPlugin',
+    'DropdownPlugin',
+    'ListGroupPlugin',
+  ]),
+})
+
+export default Config
---
+++ frontend/src/admin/pages/users/create.vue
@@ -0,0 +1,26 @@
+<template>
+  <vesp-modal v-model="record" :url="url" :title="$t('models.user.title_one')">
+    <template #form-fields>
+      <form-user v-model="record" />
+    </template>
+  </vesp-modal>
+</template>
+<script>
+import {url} from '../users'
+import FormUser from '@/components/forms/user'
+
+export {url}
+export default {
+  components: {FormUser},
+  data() {
+    return {
+      url,
+      record: {
+        fullname: '',
+        password: '',
+        active: true,
+      },
+    }
+  },
+}
+</script>
